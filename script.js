53c62366512f6da1e2eab2b81f7f9fec5a6ba100
alisamir <computernerd2655@gmail.com>
2024-11-23 16:19:07 +0200

    Adding JS to read TXT

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 // script.js
+document.getElementById('fileInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
 
+    if (file) {
+        // Read the file as text
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const fileContent = reader.result;
+            // Do something with the file content, e.g., display it in the textarea
+    
+            // Append the file content to the textarea
+            document.getElementById('textarea').innerText = e.target.result;
+        }; 
+        reader.readAsText(file);
+    } else {
+        alert("Please select a file."); 
+    }
+});
 // document.addEventListener("DOMContentLoaded", () => {
 //     const dropdown = document.querySelector(".dropdown");
 //     const dropdownList = document.querySelector(".dropdown-list");
