b61979a1a416a524b015a0b69947450a7d6dfe52
alisamirs <computernerd2655@gmail.com>
2025-04-16 22:04:34 +0200

    again

---
+++ Fyodor_Dostoevsky.mm
@@ -0,0 +1,121 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1743803101434" ID="ID_246927173" MODIFIED="1743803101434" TEXT="Fyodor Dostoevsky&apos;s Philosophy">
+<node CREATED="1743803101434" ID="ID_372392284" MODIFIED="1743803404771" POSITION="right" TEXT="Human Nature &amp; Psychology">
+<icon BUILTIN="full-1"/>
+<node CREATED="1743803101434" ID="ID_1077610658" MODIFIED="1743803431073" TEXT="Duality of good and evil within individuals">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_1068828989" MODIFIED="1743803433164" TEXT="Inner conflict and irrationality">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_1525629033" MODIFIED="1743803436359" TEXT="The &apos;underground man&apos; &#x2013; self-destructive, alienated, hyper-conscious">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_550753281" MODIFIED="1743803445684" TEXT="Depth of suffering and its role in shaping character">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_1558833134" MODIFIED="1743803481487" TEXT="Key Work: Notes from Underground">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1743803101434" ID="ID_778930506" MODIFIED="1743803720539" POSITION="right" TEXT="Free Will &amp; Responsibility">
+<icon BUILTIN="full-2"/>
+<node CREATED="1743803101434" ID="ID_1025791375" MODIFIED="1743803572765" TEXT="Rejection of determinism and rational utopias">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_1686962549" MODIFIED="1743803581809" TEXT="Belief in the sacredness of human choice&#x2014;even to choose evil">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_655947930" MODIFIED="1743803616605" TEXT="Freedom as both a burden and a blessing">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_1247469867" MODIFIED="1743803621492" TEXT="Key Work: Crime and Punishment">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1743803101434" ID="ID_328861725" MODIFIED="1743803702460" POSITION="right" TEXT="Suffering &amp; Redemption">
+<icon BUILTIN="full-3"/>
+<node CREATED="1743803101434" ID="ID_1087395822" MODIFIED="1743803646983" TEXT="Suffering as a path to spiritual awakening">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_250678441" MODIFIED="1743803656295" TEXT="Pain reveals truth and unites people">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_1143515057" MODIFIED="1743803669779" TEXT="Christ-like figures suffer for others (e.g., Prince Myshkin, Alyosha Karamazov)">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_1026360047" MODIFIED="1743803675466" TEXT="Key Works: The Idiot, The Brothers Karamazov">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1743803101434" ID="ID_205096791" MODIFIED="1743803705739" POSITION="right" TEXT="Faith vs. Doubt">
+<icon BUILTIN="full-4"/>
+<node CREATED="1743803101434" ID="ID_1727220628" MODIFIED="1743803757934" TEXT="Tension between belief in God and rejection of divine justice">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_1913259850" MODIFIED="1743803848701" TEXT="Ivan Karamazov&#x2019;s rebellion: &apos;If all suffering ends in harmony, is it worth one child&apos;s tears?&apos;">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_758660148" MODIFIED="1743804074533" TEXT="Exploration of atheism and religious faith as coexisting forces">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_337452601" MODIFIED="1743804083930" TEXT="Key Work: The Brothers Karamazov (&apos;The Grand Inquisitor&apos;)">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1743803101434" ID="ID_965860530" MODIFIED="1743803706932" POSITION="right" TEXT="Critique of Rationalism &amp; Utopianism">
+<icon BUILTIN="full-5"/>
+<node CREATED="1743803101434" ID="ID_762616445" MODIFIED="1743804101062" TEXT="Dystopian view of purely rational societies (e.g., Crystal Palace)">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_800674781" MODIFIED="1743804105698" TEXT="Warning against ideological fanaticism">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_1187413743" MODIFIED="1743804113708" TEXT="Destructive nature of imposing &apos;perfect&apos; systems on flawed humans">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_1754245997" MODIFIED="1743804121226" TEXT="Key Work: Demons (aka The Devils)">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1743803101434" ID="ID_456084015" MODIFIED="1743803708614" POSITION="right" TEXT="Moral Ambiguity &amp; Ethical Dilemmas">
+<icon BUILTIN="full-6"/>
+<node CREATED="1743803101434" ID="ID_1406905192" MODIFIED="1743804346908" TEXT="No easy heroes or villains">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_1557724714" MODIFIED="1743804352941" TEXT="Constant moral questioning: &apos;Do ends justify the means?&apos;">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_1571092276" MODIFIED="1743804441483" TEXT="Emphasis on personal conscience over external rules">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_669861390" MODIFIED="1743804454186" TEXT="Key Work: Crime and Punishment">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1743803101434" ID="ID_411468216" MODIFIED="1743803710842" POSITION="right" TEXT="Redemption Through Love">
+<icon BUILTIN="full-7"/>
+<node CREATED="1743803101434" ID="ID_1645995165" MODIFIED="1743804474667" TEXT="Compassion, humility, and forgiveness as redemptive forces">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_1120340992" MODIFIED="1743804481883" TEXT="Romantic and spiritual love as a path to God">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_1797135573" MODIFIED="1743804489226" TEXT="Love often intertwined with suffering and sacrifice">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743803101434" ID="ID_1554877923" MODIFIED="1743804497274" TEXT="Key Work: The Idiot">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+</map>
